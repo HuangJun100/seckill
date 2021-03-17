a66cf2a1c2216c54f4f78bc33fec3a439170e098
HuangJun100 <2269157059@qq.com>
2021-03-17 12:34:05 +0800

    增加redis缓存用户登录信息

--- src/main/java/com/example/seckill/controller/UserController.java
+++ src/main/java/com/example/seckill/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.example.seckill.controller;
 
 
+import com.alibaba.druid.sql.visitor.functions.If;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.example.seckill.entity.User;
 import com.example.seckill.service.UserService;
@@ -8,13 +9,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 /**
  * <p>
@@ -36,18 +40,30 @@ public class UserController {
     public String loginByUser(@RequestParam(name = "username") String username,
                               @RequestParam(name = "password") String password,
                               HttpServletRequest request,
-                              HttpServletResponse response) {
-        //查询用户是否存在
+                              HttpServletResponse response,
+                              Model model) {
+        /**
+         * 首先查看redis中是否存在用户，如果存在，直接读取并对比，如果不存在则查询数据库后对比，正确后存入redis
+         */
+        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
+            model.addAttribute("msg", "用户名或密码为空");
+            return "redirect:/home";
+        }
+        User redisUser = (User) redisTemplate.opsForValue().get("user" + username);
+        if (redisUser != null && password.equals(redisUser.getPassword())) {
+            return "hello";
+        }
         User dbUser = userService.getOne(new LambdaQueryWrapper<User>()
                 .eq(User::getNickname, username));
-        //如果存在的话，跳转hello，否则返回home页
         if (dbUser != null && dbUser.getPassword().equals(password)) {
-            redisTemplate.opsForValue().set("user:", dbUser);
+            redisTemplate.opsForValue().set("user:" + dbUser.getNickname(), dbUser);
             return "hello";
         } else {
-            return "home";
+            model.addAttribute("msg", "用户名或密码错误");
+            return "redirect:home";
         }
     }
+
 
     @GetMapping("/home")
     public String getHomePage() {
